61cfe2a319af2517d71c96667b64dde6c9feb9ce
JiungChoi <81892025+JiungChoi@users.noreply.github.com>
2021-09-23 12:53:26 +0900

    Create QueNum_1_1.py

---
+++ QueNum_1_1.py
@@ -0,0 +1,10 @@
+## 구구단표를 출력하는 프로그램을 작성하라. 형식은 정확히 일치하지 않아도 된다.
+
+
+for i in range(1, 10):
+    ary = []
+    for j in range(1, 10):
+        ary.append(i*j)
+    print(f"{ary}")
+        
+        
