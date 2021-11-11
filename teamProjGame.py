56ddfdd80ef929362aeb55447d93f06869782cc3
JiungChoi <81892025+JiungChoi@users.noreply.github.com>
2021-11-11 22:56:03 +0900

    Update teamProjGame.py

--- teamProjGame.py
+++ teamProjGame.py
@@ -1,3 +1,17 @@
+import numpy as np
+import dlib, cv2 # cv2 : webcam 
+from imutils import face_utils
+import pygame
+
+import os, sys
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+PRESENT_FRAME_WRITE_PATH = "Jiung/jiung.jpg"
+
+
+
+
 class floatElement: # 부유물 클래스: 점수 클래스(plus)와, 실점(minus) 클래스로 상속한다. 
     def __init__(self, x, y):
         self.x = x # x좌표
@@ -16,73 +30,62 @@ class minusElement(floatElement): # 실점 클래스(plus) : 부유물 객체로
 
 class Game:
     def __init__(self):
-        self.userName = input("Insert user info : ")
-        self.floatElemets = [[], []] # 0 : plusElement, 1 : minusElement
-        self.gameWindowInfo = [640, 480] # 0 : boardWidth, 1 : boardHeight ... default [width : 640, hegiht : 480]
+        #self.userName = input("Insert user info : ")
+        self.WIDTH = 800
+        self.HEIGHT = 800
+        
+
 
     def windowSet(self, width, height):
-        self.gameWindowInfo[0] = width
-        self.gameWindowInfo[1] = height
+        SCREEN_WIDTH = width
+        SCREEN_HEIGHT = height
 
     def userSet(self, name):
         self.userName = name
 
 
     def run(self):
-        print(f" hi {self.userName}! \nif you want to quit this game press \"q\"! ")
-        self.floatElemets[0].append(plusElement("path1", 50, 60))
-        self.floatElemets[0].append(plusElement("path2", 5, 6))
-        self.floatElemets[0].append(minusElement("path3", 60, 70))
-        
+        # pygame initialize
+        pygame.init()
 
-'''#       
+        SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption("Yam-Yam")
+
+
+        detector = dlib.get_frontal_face_detector()
+        predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+        keyPointModel = ('models/2018_12_17_22_58_35.h5')
+
         cap = cv2.VideoCapture(0)
         while cap.isOpened():
             ret, frame = cap.read()
-
-            if not ret:
-                break
-
+            if not ret: break
             img = frame.copy()
             gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            faces = dector(gray_img, 1)
-
+            faces = detector(gray_img, 1)
             for face in faces:
                 shapes = predictor(gray_img, face)
                 shapes = face_utils.shape_to_np(shapes)
-
                 for point in shapes:
-                    cv2.cicle(img, point, 2, (255, 255, 0))
+                    cv2.circle(img, point, 2, (255, 255, 255))
 
-            cv2.imshow("result", img)
-            if cv2.waitkey(1) == ord("q"):
-                break
+            cv2.imwrite(PRESENT_FRAME_WRITE_PATH, img)
+            img = pygame.image.load(PRESENT_FRAME_WRITE_PATH)
+            eagle_img = pygame.transform.scale(pygame.image.load("eagle.jpg"), (300,300))
 
-#'''
+            SCREEN.blit(img, [0, 0])
+            SCREEN.blit(eagle_img, [100,100])
+            
+            pygame.display.update()
+            
+            
+
+            
+
 
 
 if __name__ == "__main__":
     # generte the game
     print("Welcome to our game !!")
     game = Game()
-    while(True):
-        gameMenu = int(input("1. Start game 2. Settings 3. exit : "))
-
-        if (gameMenu == 1): game.run()
-        elif (gameMenu == 2):
-            editSetting = int(input("1. Display 2.User ... : "))
-            if(editSetting == 1):
-                while(True):
-                    try:
-                        width, height = map(int, input("Insert .. [width height] : ").split())
-                        break
-                    except ValueError:
-                        print("Insert right Value")
-
-                game.windowSet(width, height)
-            elif(editSetting == 2): game.userSet()
-            else: print("Wrong function")
-        elif(gameMenu == 3):
-            print("exit")
-            break
-        else: print("Wrong function")
+    game.run()
