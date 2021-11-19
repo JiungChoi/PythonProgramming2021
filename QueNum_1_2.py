5ff39ba9b53b7790e30fcf31351cf25ef56d9a41
JiungChoi <81892025+JiungChoi@users.noreply.github.com>
2021-11-19 15:28:15 +0900

    Update QueNum_1_2.py

--- QueNum_1_2.py
+++ QueNum_1_2.py
@@ -1,7 +1,8 @@
+## 최대 공약수를 찾는 프로그램을 작성해 보자
+
 
 #%%
 def getGCD(a, b):
-    
     while(1):    
         if (a%b):
             a, b = b, a%b           
@@ -11,5 +12,5 @@ def getGCD(a, b):
     
     
 num1, num2 = map(int, input("두 수를 입력하세요 : ").split())
-
 print( "두 수의 최대 공약수는 : ", getGCD(num1, num2), "입니다.")
+
