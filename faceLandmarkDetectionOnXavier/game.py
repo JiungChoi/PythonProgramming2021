9fce25b8b1a6fff7507799afb766f661fd7b1e5f
JiungChoi <81892025+JiungChoi@users.noreply.github.com>
2021-11-19 16:01:40 +0900

    Update game.py

--- faceLandmarkDetectionOnXavier/game.py
+++ faceLandmarkDetectionOnXavier/game.py
@@ -7,8 +7,10 @@ PRESENT_FRAME_WRITE_PATH = "Jiung\jiung.jpg"
 
 SCREEN_WIDTH_DEFAULT = 800
 SCREEN_HEIGHT_DEFAULT = 800
-BUTTON_START_HEIGHT = 100
-BUTTON_START_WIDTH = 400
+INTRO_BUTTON_LARGE_HEIGHT = 100
+INTRO_BUTTON_LARGE_WIDTH = 400
+RUN_BUTTON_SMALL_HEIGHT = 25
+RUN_BUTTON_SMALL_WIDTH = 25
 
 #### COLOR ####
 BLACK = (0, 0, 0)
@@ -17,14 +19,10 @@ BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
 YELLOW = (255, 255, 0)
 
-
-
-
-
 class Button():
     def __init__(self,BUTTON_IMG_PATH, x, y):
         self.button = pygame.image.load(BUTTON_IMG_PATH)
-        self.button = pygame.transform.scale(self.button,(BUTTON_START_WIDTH, BUTTON_START_HEIGHT))
+        self.button = pygame.transform.scale(self.button,(INTRO_BUTTON_LARGE_WIDTH, INTRO_BUTTON_LARGE_HEIGHT))
         self.rect = self.button.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -69,17 +67,21 @@ class Game:
     def gameFactor(self): # 이미지 파일들의 정보를 관리 : [경로, [x,y], (width, height))]
         self.intro_gameScreenInfo = ["intro_gameScreen.png", [0, 0] , (self.SCREEN_WIDTH,self.SCREEN_HEIGHT)]
         self.intro_chefInfo = ["chef.png", [self.SCREEN_WIDTH/2 - 0.5*self.SCREEN_WIDTH/1.6, self.SCREEN_HEIGHT- self.SCREEN_HEIGHT/2], (self.SCREEN_WIDTH/1.6, self.SCREEN_HEIGHT/2)]
-        self.intro_gameStartButtonInfo = ["intro_gameStrartButton.png", [self.SCREEN_WIDTH/2 - BUTTON_START_WIDTH/2, self.SCREEN_HEIGHT/2 - BUTTON_START_HEIGHT/2 - BUTTON_START_HEIGHT ], (BUTTON_START_WIDTH, BUTTON_START_HEIGHT)]
-        self.intro_gameSettingsButtonInfo = ["intro_gameSettingsButton.png", [self.SCREEN_WIDTH/2 - BUTTON_START_WIDTH/2, self.SCREEN_HEIGHT/2 - BUTTON_START_HEIGHT/2 ], (BUTTON_START_WIDTH, BUTTON_START_HEIGHT)]        
-
+        self.intro_gameStartButtonInfo = ["intro_gameStrartButton.png", [self.SCREEN_WIDTH/2 - INTRO_BUTTON_LARGE_WIDTH/2, self.SCREEN_HEIGHT/2 - INTRO_BUTTON_LARGE_HEIGHT/2 - INTRO_BUTTON_LARGE_HEIGHT ], (INTRO_BUTTON_LARGE_WIDTH, INTRO_BUTTON_LARGE_HEIGHT)]
+        self.intro_gameSettingsButtonInfo = ["intro_gameSettingsButton.png", [self.SCREEN_WIDTH/2 - INTRO_BUTTON_LARGE_WIDTH/2, self.SCREEN_HEIGHT/2 - INTRO_BUTTON_LARGE_HEIGHT/2 ], (INTRO_BUTTON_LARGE_WIDTH, INTRO_BUTTON_LARGE_HEIGHT)]        
+        self.run_menuButtonInfo = ["run_menuButton.png", [self.SCREEN_WIDTH*3/5- RUN_BUTTON_SMALL_WIDTH/2, 0], (RUN_BUTTON_SMALL_WIDTH, RUN_BUTTON_SMALL_HEIGHT)]
+        #self.run_menuButtonInfo = ["run"]
+    
     def getImgs(self):
         self.imgs_intro_gameScreenInfo = pygame.transform.scale(pygame.image.load(self.intro_gameScreenInfo[0]), self.intro_gameScreenInfo[2])
         self.imgs_intro_chefInfo = pygame.transform.scale(pygame.image.load(self.intro_chefInfo[0]), self.intro_chefInfo[2])
         
     
     def buttons_generate(self):
-        self.intro_gameStartButton = Button(self.intro_gameStartButtonInfo[0], self.SCREEN_WIDTH/2 - BUTTON_START_WIDTH/2, self.SCREEN_HEIGHT/2 - BUTTON_START_HEIGHT/2- BUTTON_START_HEIGHT ) # 인트로 게임 시작 버튼 생성
-        self.intro_gameSettingsButton = Button(self.intro_gameSettingsButtonInfo[0], self.SCREEN_WIDTH/2 - BUTTON_START_WIDTH/2, self.SCREEN_HEIGHT/2 - BUTTON_START_HEIGHT/2   ) #인트로 게임 설정 버튼 생성
+        self.intro_gameStartButton = Button(self.intro_gameStartButtonInfo[0], self.intro_gameStartButtonInfo[1][0], self.intro_gameStartButtonInfo[1][1]) # 인트로 게임 시작 버튼 생성
+        self.intro_gameSettingsButton = Button(self.intro_gameSettingsButtonInfo[0], self.intro_gameSettingsButtonInfo[1][0], self.intro_gameSettingsButtonInfo[1][1]) #인트로 게임 설정 버튼 생성
+        self.run_menuButton = Button(self.run_menuButtonInfo[0], self.run_menuButtonInfo[1][0], self.run_menuButtonInfo[1][1] )
+    
 
     ########## 게임 세팅 ##########
 
@@ -111,6 +113,7 @@ class Game:
             self.SCREEN.blit(self.imgs_intro_chefInfo, self.intro_chefInfo[1])
             self.SCREEN.blit(self.intro_gameStartButton.button, self.intro_gameStartButtonInfo[1])
             self.SCREEN.blit(self.intro_gameSettingsButton.button, self.intro_gameSettingsButtonInfo[1])
+            
 
             pygame.display.update()
             self.clock.tick(15)
@@ -127,6 +130,8 @@ class Game:
             self.gameBoard()
             
             self.SCREEN.blit(self.img, [0, 0])
+            self.SCREEN.blit(self.run_menuButton.button, self.run_menuButtonInfo[1])
+            
             pygame.display.update()
 
     ######## 게임 프로그램 실행 ########
@@ -134,9 +139,9 @@ class Game:
 
         pygame.init() # 파이게임 라이브러리 초기 세팅
         self.clock = pygame.time.Clock() # timmer 사용을 위한 객체 생성 
-        self.gameFactor()  
-        self.getImgs()
-        self.buttons_generate()
+        self.gameFactor()  # 어떤 요소를 만들지 선언
+        self.getImgs() # gameFactor에서 선언한 요소의 이미지 파일을 불러들인다. [객체는 제외  Ex) Button ]
+        self.buttons_generate() # 버튼 생성 메서드
 
         self.SCREEN = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT)) 
         pygame.display.set_caption("Yam-Yam")
